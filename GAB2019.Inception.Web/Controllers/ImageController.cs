015af86af385063a62273537aca084bed5312812
Santiago Porras Rodríguez <santypr@gmail.com>
2019-04-24 16:01:26 +0200

    First web structure

---
+++ GAB2019.Inception.Web/Controllers/ImageController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GAB2019.Inception.Web.Middleware;
+using GAB2019.Inception.Web.Models.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace GAB2019.Inception.Web.Controllers
+{
+    public class ImageController : Controller
+    {
+        // make sure that appsettings.json is filled with the necessary details of the azure storage
+        private readonly AzureStorageSettings storageConfig = null;
+
+        public ImageController(IOptions<AzureStorageSettings> config)
+        {
+            storageConfig = config.Value;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // POST /images/upload
+        [HttpPost()]
+        public async Task<IActionResult> Upload(ICollection<IFormFile> files)
+        {
+            bool isUploaded = false;
+
+            try
+            {
+                if (files.Count == 0)
+                    return BadRequest("No files received from the upload");
+                if (storageConfig.AccountKey == string.Empty || storageConfig.AccountName == string.Empty)
+                    return BadRequest("sorry, can't retrieve your azure storage details from appsettings.js, make sure that you add azure storage details there");
+                if (storageConfig.ImageContainer == string.Empty)
+                    return BadRequest("Please provide a name for your image container in the azure blob storage");
+
+                foreach (var formFile in files)
+                {
+                    if (AzureStorage.IsImage(formFile))
+                    {
+                        if (formFile.Length > 0)
+                        {
+                            using (Stream stream = formFile.OpenReadStream())
+                            {
+                                isUploaded = await AzureStorage.UploadFileToStorage(stream, formFile.FileName, storageConfig);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return new UnsupportedMediaTypeResult();
+                    }
+                }
+
+                if (isUploaded)
+                {
+                    if (storageConfig.ThumbnailContainer != string.Empty)
+                        return new AcceptedAtActionResult("GetThumbNails", "Image", null, null);
+                    else
+                        return new AcceptedResult();
+                }
+                else
+                    return BadRequest("Look like the image couldnt upload to the storage");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET /images/thumbnails
+        [HttpGet()]
+        public async Task<IActionResult> GetThumbNails()
+        {
+            try
+            {
+                if (storageConfig.AccountKey == string.Empty || storageConfig.AccountName == string.Empty)
+                    return BadRequest("sorry, can't retrieve your azure storage details from appsettings.js, make sure that you add azure storage details there");
+                if (storageConfig.ImageContainer == string.Empty)
+                    return BadRequest("Please provide a name for your image container in the azure blob storage");
+
+                List<string> thumbnailUrls = await AzureStorage.GetThumbNailUrls(storageConfig);
+
+                return new ObjectResult(thumbnailUrls);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ GAB2019.Inception.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GAB2019.Inception.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
