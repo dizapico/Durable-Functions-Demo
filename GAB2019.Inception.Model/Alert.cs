50c937651fdc11c794763b7081182bc1c257533b
Diego Zapico Ferreiro <d.zapico@hotmail.com>
2019-04-25 10:08:24 +0200

    Added SendGrid function. Trigger with cosmosdb collection and send email

---
+++ GAB2019.Inception.Model/Alert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GAB2019.Inception.Model
+{
+    public class Alert
+    {
+        public string Id { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string ImageURL { get; set; }
+        public string AlertTo { get; set; }
+    }
+}
